0ab831c5e2cac62b36fa5f8380179560b3a226ac
Anija-Khallil Robinson <nastynij@Anija-Khallils-iMac.local>
2024-07-24 22:55:26 -0400

    initial commit

---
+++ color-picker/src/App.jsx
@@ -0,0 +1,28 @@
+
+import { useState } from "react";
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
+  const Color =({color, setSelectedColor}) =>{
+    return <div className={color} onClick ={
+      () => setSelectedColor(color)}></div>;
+  };
+  return (
+    <div id="container">
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+      <div id="colors-list">
+        <Color color="black" setSelectedColor={setSelectedColor}/>
+        <Color color="blue"setSelectedColor={setSelectedColor}/>
+        <Color color="orange"setSelectedColor={setSelectedColor}/>
+    </div>
+    </div>
+     
+     );
+};
+
+<div>
+
+</div>
+export default App;
